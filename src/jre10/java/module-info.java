c59ca1c79ee67ba9fef2718685eee8a3c46ae945
GedMarc <ged_marc@hotmail.com>
2018-11-05 21:39:36 +0200

    Inclusions and Exclusions, base package change to correct for jquery ui themes, bootstrap tags update, smart wizard split for bootstrap 4

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -1,5 +1,8 @@
 import com.jwebmp.core.services.IPageConfigurator;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 import com.jwebmp.plugins.bs4datetimepicker.BS4DateTimePageConfigurator;
+import com.jwebmp.plugins.bs4datetimepicker.implementations.BS4DateTimePickerExclusionsModule;
 
 module com.jwebmp.plugins.bs4datetimepicker {
 	exports com.jwebmp.plugins.bs4datetimepicker;
@@ -13,11 +16,15 @@ module com.jwebmp.plugins.bs4datetimepicker {
 
 	requires java.validation;
 	requires com.jwebmp.plugins.bootstrap4;
+	requires com.jwebmp.guicedinjection;
 
 	provides IPageConfigurator with BS4DateTimePageConfigurator;
 
-	opens com.jwebmp.plugins.bs4datetimepicker to com.fasterxml.jackson.databind,com.jwebmp.core;
-	opens com.jwebmp.plugins.bs4datetimepicker.interfaces to com.fasterxml.jackson.databind,com.jwebmp.core;
-	opens com.jwebmp.plugins.bs4datetimepicker.events to com.fasterxml.jackson.databind,com.jwebmp.core;
-	opens com.jwebmp.plugins.bs4datetimepicker.options to com.fasterxml.jackson.databind,com.jwebmp.core;
+	provides IGuiceScanJarExclusions with BS4DateTimePickerExclusionsModule;
+	provides IGuiceScanModuleExclusions with BS4DateTimePickerExclusionsModule;
+
+	opens com.jwebmp.plugins.bs4datetimepicker to com.fasterxml.jackson.databind, com.jwebmp.core;
+	opens com.jwebmp.plugins.bs4datetimepicker.interfaces to com.fasterxml.jackson.databind, com.jwebmp.core;
+	opens com.jwebmp.plugins.bs4datetimepicker.events to com.fasterxml.jackson.databind, com.jwebmp.core;
+	opens com.jwebmp.plugins.bs4datetimepicker.options to com.fasterxml.jackson.databind, com.jwebmp.core;
 }
---
+++ src/main/java/com/jwebmp/plugins/bs4datetimepicker/implementations/BS4DateTimePickerExclusionsModule.java
@@ -0,0 +1,30 @@
+package com.jwebmp.plugins.bs4datetimepicker.implementations;
+
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
+
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+public class BS4DateTimePickerExclusionsModule
+		implements IGuiceScanModuleExclusions<BS4DateTimePickerExclusionsModule>,
+				           IGuiceScanJarExclusions<BS4DateTimePickerExclusionsModule>
+{
+
+	@Override
+	public @NotNull Set<String> excludeJars()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("jwebmp-bootstrap4-date-time-picker-*");
+		return strings;
+	}
+
+	@Override
+	public @NotNull Set<String> excludeModules()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("com.jwebmp.plugins.bs4datetimepicker");
+		return strings;
+	}
+}
